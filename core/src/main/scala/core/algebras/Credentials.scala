2b93b0c4fc4bf45af3609712757ccf965ba99100
Oscar De Alva <oscar.ivan98@gmail.com>
2020-10-27 11:19:44 -0600

    Generalized reading of Credentials to be used for other types (Mainly for email reading, TBA).

--- core/src/main/scala/core/algebras/Credentials.scala
+++ core/src/main/scala/core/algebras/Credentials.scala
@@ -3,7 +3,7 @@ package core.algebras
 import core.model.errors.PocketError
 import core.model.credentials.{PocketCredentials, PocketUseData}
 
-trait Credentials[F[_]] {
-  def readCredentials: F[Either[PocketError, PocketUseData]]
+trait Credentials[F[_], A] {
+  def readCredentials: F[Either[PocketError, A]]
   def saveCredentials(saveCredentials: PocketCredentials): F[Either[PocketError, PocketCredentials]]
 }

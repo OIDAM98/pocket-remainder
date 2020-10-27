efd1c28772ddd5c6e7fa7f037c9f9dc79c822174
Oscar De Alva <oscar.ivan98@gmail.com>
2020-10-27 12:54:17 -0600

    Added MailFileInterpreter to read mailing credentials from specified file.

---
+++ core/src/main/scala/core/interpreters/inmemory/MailFileInterpreter.scala
@@ -0,0 +1,37 @@
+package core.interpreters.inmemory
+
+import cats.implicits._
+import cats.effect.Sync
+import core.algebras.Credentials
+import core.model.credentials.MailCredentials
+import core.model.errors.{NoFileFound, PocketError, UnexpectedError}
+import io.circe.parser._
+import core.model.json.decoders._
+import core.model.json.encoders._
+import io.circe.syntax.EncoderOps
+
+import scala.util.{Failure, Success, Try}
+
+final class MailFileInterpreter[F[_]: Sync] private (filename: String)
+    extends Credentials[F, MailCredentials, MailCredentials] {
+  def readCredentials: F[Either[PocketError, MailCredentials]] =
+    Sync[F].delay(Try(os.read(os.pwd / filename))).map {
+      case Success(value) =>
+        decode[MailCredentials](value).leftMap(e => UnexpectedError(e.getMessage))
+      case Failure(_) => Either.left(NoFileFound)
+    }
+
+  def saveCredentials(
+      saveCredentials: MailCredentials
+  ): F[Either[PocketError, MailCredentials]] =
+    Sync[F]
+      .delay(
+        Try(
+          os.write.over(
+            os.pwd / filename,
+            saveCredentials.asJson.spaces2
+          )
+        )
+      )
+      .map(_.toEither.map(_ => saveCredentials).leftMap(e => UnexpectedError(e.getMessage)))
+}

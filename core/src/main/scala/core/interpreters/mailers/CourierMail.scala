14da6c3705ec1945c4128832776aebdda82dad92
Oscar De Alva <oscar.ivan98@gmail.com>
2020-10-27 18:54:43 -0600

    Added CourierMail as Interpreter for sending PocketArticles by email.

---
+++ core/src/main/scala/core/interpreters/mailers/CourierMail.scala
@@ -0,0 +1,48 @@
+package core.interpreters.mailers
+
+import cats.effect.Async
+import core.algebras.MailService
+import cats.implicits._
+import core.model.credentials.MailCredentials
+import core.model.responses
+import courier._
+import Defaults._
+import core.model.errors.PocketError
+import core.utilities.pocket
+
+import scala.util.{Failure, Success}
+
+final class CourierMail[F[_]: Async] private (credentials: MailCredentials) extends MailService[F] {
+  private val mailer: F[Mailer] = Async[F].delay(
+    Mailer("smtp.gmail.com", 465)
+      .auth(true)
+      .as(credentials.email, credentials.password)
+      .ssl(true)()
+  )
+  def send(email: String, articles: List[responses.PocketArticle]): F[Either[PocketError, String]] =
+    for {
+      mail <- mailer
+      envelope =
+        Envelope
+          .from(credentials.email.addr)
+          .to(email.addr)
+          .subject("Pocket Remainder for the week")
+          .content(
+            Multipart()
+              .text("These are the articles for the week:\n\n")
+              .html(pocket.createEmailBody(articles))
+          )
+      response <- Async[F].async { cb: (Either[Throwable, String] => Unit) =>
+        mail(envelope).onComplete {
+          case Success(_)         => cb(Right(s"Successfully sent message to $email!"))
+          case Failure(exception) => cb(Left(exception))
+        }
+      }
+    } yield response.asRight
+
+}
+
+object CourierMail {
+  def apply[F[_]: Async](credentials: MailCredentials): F[CourierMail[F]] =
+    Async[F].delay(new CourierMail[F](credentials))
+}

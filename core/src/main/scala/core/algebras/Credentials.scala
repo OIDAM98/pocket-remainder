791edad35088cab8c698cb9f15e9948f7f785dcd
Oscar De Alva <oscar.ivan98@gmail.com>
2021-06-12 12:44:22 -0500

    Deleted CredentialsType from model and algebra

--- core/src/main/scala/core/algebras/Credentials.scala
+++ core/src/main/scala/core/algebras/Credentials.scala
@@ -1,9 +1,8 @@
 package core.algebras
 
 import core.model.errors.PocketError
-import core.model.credentials.{CredentialsType, PocketUseData}
 
-trait Credentials[F[_], A, B <: CredentialsType] {
+trait Credentials[F[_], A, B <: A] {
   def readCredentials: F[Either[PocketError, A]]
   def saveCredentials(saveCredentials: B): F[Either[PocketError, B]]
 }
